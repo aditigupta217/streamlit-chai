1f2e15ca49a6d70554c7a53177888e0ca2e2ad1d
aditigupta217 <aditigupta221177@gmail.com>
2025-08-01 22:51:08 +0530

    day1

---
+++ main.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title("Hello to All of them!!!")
+st.subheader("Brewed with streamlit")
+st.text("Welcome to first interactive app")
+st.write("Select your fav. icecreamuv ")
